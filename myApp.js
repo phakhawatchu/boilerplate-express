3ecc9163830761ed9d0408becd39738c0f70aca9
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2022-06-25 12:55:57 +0700

    chore: add get Hello Express

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello World");
+app.get("/", (req, res) => {
+    console.log("Hello Express");
+    res.send("Hello Express");
+});
 
 module.exports = app;
