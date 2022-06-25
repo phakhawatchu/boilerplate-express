85d8e0561daed357a19b79c37a991d61d6cfa228
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2022-06-25 13:45:14 +0700

    feat: add condition in json

--- myApp.js
+++ myApp.js
@@ -11,7 +11,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-    res.json({ message: "Hello json" });
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+        res.json({ message: "HELLO JSON" });
+    } else {
+        res.json({ message: "Hello json" });
+    }
 });
 
 module.exports = app;
