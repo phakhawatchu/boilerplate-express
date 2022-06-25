cc2fb988444805852a038a5de6757975d578e89c
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2022-06-25 13:37:12 +0700

    feat: add static assets

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ let app = express();
 
 const absolutePath = __dirname + "/views/index.html";
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
     console.log("Hello Express");
     res.sendFile(absolutePath);
