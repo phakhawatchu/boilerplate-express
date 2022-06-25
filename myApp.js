f27a1de659b2993efc55fca28725983e4aada1e3
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2022-06-25 13:03:27 +0700

    feat: add html response in get

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 let express = require("express");
 let app = express();
 
+const absolutePath = __dirname + "/views/index.html";
+
 app.get("/", (req, res) => {
     console.log("Hello Express");
-    res.send("Hello Express");
+    res.sendFile(absolutePath);
 });
 
 module.exports = app;
